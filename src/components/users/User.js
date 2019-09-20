9d81ed1c694cf6e39172211816309be3a0560601
Carneirinho <carneirinho@indice.in>
2019-09-20 18:42:50 -0300

    Added Repos without Hooks

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,5 +1,6 @@
 import React, { Fragment, Component } from "react";
 import Spinner from "../layout/Spinner";
+import Repos from "../repos/Repos";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
@@ -7,12 +8,13 @@ export class User extends Component {
   componentDidMount() {
     // Get the login passed by props
     this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool.isRequired,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
     getUserRepos: PropTypes.func.isRequired
   };
@@ -35,7 +37,7 @@ export class User extends Component {
       hireable
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />;
     return (
@@ -101,6 +103,7 @@ export class User extends Component {
           <div className="badge badge-light">Public repos: {public_repos}</div>
           <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
